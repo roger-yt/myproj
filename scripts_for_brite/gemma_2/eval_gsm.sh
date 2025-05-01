1364cf07611efe5b7fdc852a34b9b8e06a7295da
YYT-t <yutongyin2028@u.northwestern.edu>
2025-05-01 14:27:11 -0500

    upload gemma script

---
+++ scripts_for_brite/gemma_2/eval_gsm.sh
@@ -0,0 +1,9 @@
+export HF_TOKEN=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
+export CUDA_VISIBLE_DEVICES="1"
+
+
+PROMPT_TYPE=gemma-it
+MODEL_NAME_OR_PATH=YYT-t/math_gsm-gemma-2-9b-it-m-iter-1_sample_7500_nsk_ml256_mlr5e-5_ent0.0
+DATA_NAME="gsm8k"
+cd evaluation
+bash sh/my_eval.sh $PROMPT_TYPE $MODEL_NAME_OR_PATH $DATA_NAME
