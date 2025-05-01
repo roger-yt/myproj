7cbb7947e068b568ff6bf11091a3e7fbc81738c5
YYT-t <yutongyin2028@u.northwestern.edu>
2025-05-01 14:04:21 -0500

    l

--- scripts_for_brite/llama/eval_gsm.sh
+++ scripts_for_brite/llama/eval_gsm.sh
@@ -5,4 +5,5 @@ export CUDA_VISIBLE_DEVICES="1"
 PROMPT_TYPE=llama-3-8b-it
 MODEL_NAME_OR_PATH=YYT-t/math_gsm-Meta-Llama-3-8B-Instruct-m-iter-1_sample_7500_nsk_ml512_mlr5e-5_ent0.0
 DATA_NAME="gsm8k"
-bash evaluation/sh/my_eval.sh $PROMPT_TYPE $MODEL_NAME_OR_PATH $DATA_NAME
+cd evaluation
+bash sh/my_eval.sh $PROMPT_TYPE $MODEL_NAME_OR_PATH $DATA_NAME
