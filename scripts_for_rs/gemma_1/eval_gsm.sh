2193ed04de0f2097283e66097699f7017ff14794
YYT-t <yutongyin2028@u.northwestern.edu>
2025-05-02 12:48:27 -0500

    eval gsm

---
+++ scripts_for_rs/gemma_1/eval_gsm.sh
@@ -0,0 +1,9 @@
+export HF_TOKEN=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
+export CUDA_VISIBLE_DEVICES="1"
+
+
+PROMPT_TYPE=gemma-it_box
+MODEL_NAME_OR_PATH=YYT-t/math_gsm-gemma-1.1-7b-it-rs-sample_7500_temp_1.0_gen_30_mlr5e-5
+DATA_NAME="gsm8k"
+cd evaluation
+bash sh/my_eval.sh $PROMPT_TYPE $MODEL_NAME_OR_PATH $DATA_NAME
